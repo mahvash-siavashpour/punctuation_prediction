cf2a479a869154026a257064d69f7d95446dee22
Mahvash Siavashpour <mahvash.siavashpour@gmail.com>
2023-03-11 12:30:21 +0330

    minor changes

--- src/CNN-LSTM/main.py
+++ src/CNN-LSTM/main.py
@@ -258,6 +258,10 @@ def train_one_epoch(epoch_index, tb_writer):
             new_y = y.detach().numpy()
             new_outputs = outputs.detach().numpy()
             new_outputs = np.argmax(new_outputs,axis=2)
+
+            print(new_outputs.reshape(-1).tolist())
+            print(new_y.reshape(-1).tolist())
+
             results = metric.compute(predictions=new_outputs.reshape(-1).tolist(), references=new_y.reshape(-1).tolist())
             # f1 = f1_score(y_true=new_y.reshape(-1), y_pred=new_outputs.reshape(-1), average='macro') 
             f1 = results['overall_f1']
@@ -311,8 +315,7 @@ for epoch in range(EPOCHS):
         new_vlabels = vlabels.detach().numpy()
         new_voutputs = voutputs.detach().numpy()
         new_voutputs = np.argmax(new_voutputs,axis=2)
-        print(new_voutputs.reshape(-1).tolist())
-        print(new_vlabels.reshape(-1).tolist())
+       
         results = metric.compute(predictions=new_voutputs.reshape(-1).tolist(), references=new_vlabels.reshape(-1).tolist())
         f1 = results['overall_f1']
         # f1 = f1_score(y_true=new_vlabels.reshape(-1), y_pred=new_voutputs.reshape(-1), average='macro') 
