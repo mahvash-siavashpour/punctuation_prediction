936e49042d6ccb578ab56d9c49c70cb70e821804
Mahvash Siavashpour <mahvash.siavashpour@gmail.com>
2023-03-15 13:04:13 +0330

    minor changes

--- src/pre-processing.py
+++ src/pre-processing.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ptCZnCZzaBDVG_DExfnXL3b-FZAedWSa
 """
 
-root = "/Data/"
+root = "../Data/"
 
 import pandas as pd
 import torch
